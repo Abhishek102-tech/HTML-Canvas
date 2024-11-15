568add9dc3533b9254123fe5226ea4789e9fba51
Abhishek102-tech <181866251+Abhishek102-tech@users.noreply.github.com>
2024-11-15 23:24:26 +0530

    making new text

--- Chap 2/text.js
+++ Chap 2/text.js
@@ -1,1 +1,5 @@
-let canvas=document.querySelector(".text")
+let canvas=document.querySelector(".Text")
+let ctx=canvas.getContext("2d");
+ctx.font='30px Arial';
+// ctx.fillText("Hello World",10,50);
+ctx.strokeText("Hello World",10,50);
