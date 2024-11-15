267b0d5453414bdb99c2f0388ee9f70a636ff66d
Abhishek102-tech <181866251+Abhishek102-tech@users.noreply.github.com>
2024-11-15 23:29:55 +0530

    new text

--- Chap 2/text.js
+++ Chap 2/text.js
@@ -1,5 +1,7 @@
 let canvas=document.querySelector(".Text")
 let ctx=canvas.getContext("2d");
-ctx.font='30px Arial';
+ctx.font='30px Comic Sans MS';
 // ctx.fillText("Hello World",10,50);
-ctx.strokeText("Hello World",10,50);
+ctx.fillStyle="red";
+ctx.textAlign='center';
+ctx.fillText("Hello World",canvas.width/2,canvas.height/2);
